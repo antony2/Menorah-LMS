b24cd628eb06d19939657d7aac7fd1c0f16ac814
antony2 <antonyrokr@gmail.com>
2019-10-22 18:49:03 +0530

    added all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
+import { TutorsComponent } from './tutors/tutors.component';
+import { TutsComponent } from './tuts/tuts.component';
 
+var rules = [
+  {
+    path: 'homepage',
+    component: HomepageComponent
+  },
+  {
+    path: 'coursespage',
+    component: CoursesComponent
+  },
+  {
+    path: 'tutorspage',
+    component: TutorsComponent
+  },
+  {
+    path: 'tutspage',
+    component: TutsComponent
+  },
+  {
+    path: '',
+    component: HomepageComponent
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    CoursesComponent,
+    TutorsComponent,
+    TutsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule, RouterModule.forRoot(rules)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/tuts/tuts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tuts',
+  templateUrl: './tuts.component.html',
+  styleUrls: ['./tuts.component.css']
+})
+export class TutsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
